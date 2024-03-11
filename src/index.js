c206afb7bb11cbe190711ab718a55da24cf51ae6
LewdHuTao <imanimtiyaz242@gmail.com>
2024-03-11 09:37:38 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const { Connection } = require("./structures/niizukiConnection");
 const { Node } = require("./structures/niizukiNode");
-const { niizuki } = require("./structures/niizuki");
+const { Niizuki } = require("./structures/Niizuki");
 const { Player } = require("./structures/niizukiPlayer");
 const { Queue } = require("./structures/niizukiQueue");
 const { Rest } = require("./structures/niizukiRest");
 const { Track } = require("./structures/niizukiTrack");
 
-module.exports = { Connection, Node, niizuki, Player, Queue, Rest, Track };
+module.exports = { Connection, Node, Niizuki, Player, Queue, Rest, Track };
